55410e7527588c8875b7c44be248c8dd8a78d668
Konstantin Yavichev <kyavichev@gmail.com>
2024-11-25 12:43:48 -0800

    ModelStore improvements

--- BearEssentials/Assets/Scripts/Services/ModelStore/Interfaces/IModelStore.cs
+++ BearEssentials/Assets/Scripts/Services/ModelStore/Interfaces/IModelStore.cs
@@ -1,9 +1,35 @@
+using JetBrains.Annotations;
+
 namespace Bears.Core
 {
+    [PublicAPI]
     public interface IModelStore
     {
+        /// <summary>
+        /// Adds a model by key
+        /// </summary>
         void AddModel<T>(string key, T model) where T : IModel;
+        /// <summary>
+        /// Removes a model by key
+        /// </summary>
         void RemoveModel(string key);
+        /// <summary>
+        /// Returns a model of a type by key
+        /// </summary>
         T GetModel<T>(string key) where T : IModel;
+        
+        /// <summary>
+        /// Adds a default model for a type. Default models use a key based on the model's type.
+        /// Technically, they can also be added/removed/retrieved by key because all models are stored in a single dictionary.
+        /// </summary>
+        void AddModel<T>(T model) where T : IModel;
+        /// <summary>
+        /// Remove the default model for a type
+        /// </summary>
+        void RemoveModel<T>() where T : IModel;
+        /// <summary>
+        /// Returns the default model for a type
+        /// </summary>
+        T GetModel<T>() where T : IModel;
     }
 }
--- BearEssentials/Assets/Scripts/Services/ModelStore/Service/ModelStoreService.cs
+++ BearEssentials/Assets/Scripts/Services/ModelStore/Service/ModelStoreService.cs
@@ -1,26 +1,23 @@
-﻿namespace Bears.Core.ModelStoreInternal
+namespace Bears.Core.ModelStoreInternal
 {
-    public class ModelStoreService : ServiceComponent<IModelStoreService, ModelStoreService>, IModelStoreService
+    internal class ModelStoreService : ServiceComponent<IModelStoreService, ModelStoreService>, IModelStoreService
     {
-        private readonly ModelStore _store = new ModelStore();
+        private readonly ModelStore _store = new();
 
         protected override void OnRegister()
         {
-            
         }
 
         protected override void OnDeregister()
         {
-            
         }
 
-        public void AddModel<T>(string key, T model) where T : IModel =>
-            _store.AddModel(key, model);
-
-        public void RemoveModel(string key) =>
-            _store.RemoveModel(key);
-
-        public T GetModel<T>(string key) where T : IModel =>
-            _store.GetModel<T>(key);
+        public void AddModel<T>(string key, T model) where T : IModel => _store.AddModel(key, model);
+        public void RemoveModel(string key) => _store.RemoveModel(key);
+        public T GetModel<T>(string key) where T : IModel => _store.GetModel<T>(key);
+        
+        public void AddModel<T>(T model) where T : IModel => _store.AddModel(model);
+        public void RemoveModel<T>() where T : IModel => _store.RemoveModel<T>();
+        public T GetModel<T>() where T : IModel => _store.GetModel<T>();
     }
 }
